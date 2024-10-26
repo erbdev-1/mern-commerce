3a53b303719942e289fa147df8110f14cbb9e863
erbdev <erbdev@hotmail.com>
2024-10-26 17:19:30 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Policy from "./components/Policy";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Policy />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
