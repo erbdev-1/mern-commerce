c917f1b1232b5b3f3796753e048678a358c6541b
erbdev <erbdev@hotmail.com>
2024-10-28 15:07:06 +0000

    Refactor Header component to include search functionality

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,11 +1,15 @@
 import Footer from "../components/Layout/Footer/Footer";
 import Header from "../components/Layout/Header/Header";
 import PropTypes from "prop-types";
+import Search from "../components/Modals/Search/Search";
+import { useState } from "react";
 
 const MainLayout = ({ children }) => {
+  const [isSearchShow, setIsSearchShow] = useState(false);
   return (
-    <div>
-      <Header />
+    <div className="main-layout">
+      <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
+      <Header setIsSearchShow={setIsSearchShow} />
       {children}
       <Footer />
     </div>
