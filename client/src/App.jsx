66ad18f41fc5f1f2e432b7cf6ed10c04b7d677f7
erbdev <erbdev@hotmail.com>
2024-10-28 11:55:15 +0000

    Refactor app component and add contact and shop pages
    Closes #123

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import HomePage from "./pages/HomePage";
+import ShopPage from "./pages/ShopPage";
 import "./App.css";
 import React from "react";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
     <React.Fragment>
-      <HomePage />
+      {/* <HomePage /> */}
+      {/* <ShopPage /> */}
+      <ContactPage />
     </React.Fragment>
   );
 }
--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -1,5 +1,18 @@
+import { Fragment } from "react";
+import Header from "../components/Layout/Header/Header";
+import Policy from "../components/Layout/Policy/Policy";
+import Footer from "../components/Layout/Footer/Footer";
+import Contact from "../components/Contact/Contact";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <Fragment>
+      <Header />
+      <Contact />
+      <Policy />
+      <Footer />
+    </Fragment>
+  );
 };
 
 export default ContactPage;
