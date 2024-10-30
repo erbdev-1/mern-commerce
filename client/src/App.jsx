55fcf101f15ced1745610df760c02b9d65be9861
erbdev <erbdev@hotmail.com>
2024-10-30 13:10:30 +0000

    Install React-router-dom and determining route pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-// import ShopPage from "./pages/ShopPage";
-import "./App.css";
-import React from "react";
-// import ContactPage from "./pages/ContactPage";
-// import AuthPage from "./pages/AuthPage";
+import ShopPage from "./pages/ShopPage";
+import BlogPage from "./pages/BlogPage";
+import ContactPage from "./pages/ContactPage";
 import CartPage from "./pages/CartPage";
-
-// import BlogDetailPage from "./pages/BlogDetailPage";
-// import ProductDetailsPage from "./pages/ProductDetailsPage";
+import AuthPage from "./pages/AuthPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import BlogDetailPage from "./pages/BlogDetailPage";
+import "./App.css";
 
 function App() {
   return (
-    <React.Fragment>
-      {/* <HomePage /> */}
-      {/* <ShopPage /> */}
-      {/* <ContactPage /> */}
-      {/* <AuthPage /> */}
-      <CartPage />
-      {/* <BlogDetailPage /> */}
-      {/* <ProductDetailsPage /> */}
-    </React.Fragment>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/shop" element={<ShopPage />} />
+      <Route path="/blog" element={<BlogPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/product/:id" element={<ProductDetailsPage />} />
+      <Route path="/blog/:id" element={<BlogDetailPage />} />
+    </Routes>
   );
 }
 
--- client/src/components/Layout/Header/Header.jsx
+++ client/src/components/Layout/Header/Header.jsx
@@ -156,7 +156,7 @@ function Header({ setIsSearchShow }) {
                         </div>
                         <div className="megamenu-single">
                           <a href="#">
-                            <img src="img/mega-menu.jpg" alt="" />
+                            <img src="/img/mega-menu.jpg" alt="" />
                           </a>
                           <h3 className="megamenu-single-title">
                             JOIN THE LAYERING GANG
